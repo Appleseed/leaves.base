2ab8b896a6ea704c618298d01cf2bc4f04c1eef2
JSBILGI <jsbilgi@yahoo.com>
2018-01-25 16:12:08 +0530

    uploaded from different folder. No change

--- collect.awesome.md.csvpy.bat
+++ collect.awesome.md.csvpy.bat
@@ -1,4 +1,6 @@
 curl %1 -o %2.md
-md_to_json -o %2.json %2.md
+python html_to_markdown.py %2
+md_to_json -o %2.json %2_cp.md
 python transform.awesome.json.csv.py %2.json %2.csv
+python Get_awesome_list.py %2.csv %2.txt
 Rem cat %2.csv | tr -d ' ' | cut -d',' -f1 > awesome_list.txt
