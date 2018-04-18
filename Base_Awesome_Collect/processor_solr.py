f53c9ce44d180bc5e169091803e7effa631c3b02
JSBILGI <jsbilgi@yahoo.com>
2018-04-19 03:21:49 +0530

    Included code for extracting readable conetnt from site. Same is used for index. Otherwise even tags are indexed.

--- Base_Awesome_Collect/processor_solr.py
+++ Base_Awesome_Collect/processor_solr.py
@@ -1,3 +1,5 @@
+# Written by Jagannath Bilgi <jsbilgi@yahoo.com>
+
 from urllib.parse import urlparse
 
 import configparser
@@ -8,6 +10,8 @@ import time
 
 import sys
 import json
+
+import bs4_extract_body
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -49,15 +53,34 @@ def titleize(chars):
   return "".join(result) 
 id_val = ""
 post_docs = {}
+
+"""
+ingest_solr process is created index document retrived from wallabag api. 
+new column content_text is created to hold only readable text from sites.
+   
+"""
 def ingest_solr(doc):
    for key, value in doc.items():
-       if key == 'tags':
+       if key == 'content':
+           try:
+                content_text = bs4_extract_body.text_from_html(value)
+           except:
+               pass
+           post_docs[key] = value
+           post_docs['content_text'] = content_text
+       elif key == 'tags':
+           tags = []
+           slugs = []
            for i in value:
                for key1, value1 in i.items():
                    if key1 == "label":
-                       post_docs[key] = value1
+                       tags.append(value1)
                    elif key1 == "slug":
-                       post_docs["slug"] = value1
+                       slugs.append(value1)
+           post_docs[key] = tags
+           post_docs["slugs"] = slugs
+       # elif key == 'published_by':
+       #     publisher = []
        elif key == "_links":
            for key1, value1 in value.items():
                for key2, value2 in value1.items():
@@ -73,6 +96,5 @@ def ingest_solr(doc):
 
    try:
         res = requests.get(POST_EP + '/select?q = id:' + str(id_val) + ' & wt = json & indent = true ')
-        print(res)
    except Exception as e:
        print("Error ", str(e))
